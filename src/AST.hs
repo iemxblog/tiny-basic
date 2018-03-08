cf535c7b65d690d6c9ab82da4dfbbd5562d671ff
iemxblog <iemxblog@gmail.com>
2018-03-08 16:45:42 +0100

    Corrected a bug

--- src/AST.hs
+++ src/AST.hs
@@ -67,7 +67,7 @@ instance Show Statement where
     show (Print xs) = "PRINT " ++ (concat . intersperse ", " . map show) xs
     show (If e1 ro e2 s) = "IF " ++ show e1 ++ " " ++ show ro ++ " " ++ show e2 ++ " THEN " ++ show s
     show (Goto e) = "GOTO " ++ show e
-    show (Input xs) = "INPUT " ++ (concat . intersperse ", " . map show) xs
+    show (Input xs) = "INPUT " ++ (concat . intersperse ", " . map (\x -> [x])) xs
     show (Let v e) = "LET " ++ [v] ++ " = " ++ show e
     show (GoSub e) = "GOSUB " ++ show e
     show Return = "RETURN"
